79b2c0d22fec9ac403ba49b538f84090a2a201c9
MairaSajjad <mairasajjad2001@gmail.com>
2024-03-14 13:44:54 +0500

    starting

---
+++ src/app/(main)/(routes)/report/profitLossReport/_actions/index.ts
@@ -0,0 +1,47 @@
+import { getServerAuthSession } from "@/lib/auth"
+import { AddUserInput } from "@/validators/add-user"
+import axios from "axios"
+
+export const getAllSellProduct = async () => {
+    try {
+        const session = await getServerAuthSession()
+        const { token, business_id, customer_id} = session?.user!
+        const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/product/sell/report?&order_status=open`, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+            params: {
+                business_id: business_id,
+                customer_id: customer_id,
+                per_page: -1,
+                
+            },
+        })
+
+        return data.data
+    } catch (error) {
+        throw error
+    }
+}
+
+export const getSpecificUser = async (id: number) => {
+    try {
+        const session = await getServerAuthSession()
+        const { token, business_id } = session?.user!
+        const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/customer/${id}`, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+            params: {
+                business_id: business_id,
+            },
+        })
+
+        return data.data
+    } catch (error) {
+        throw error
+    }
+}
+
--- src/app/(main)/(routes)/report/purchaseReport/page.tsx
+++ src/app/(main)/(routes)/report/purchaseReport/page.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import { getAllSellProduct } from "./_actions";
+import { OrderT } from "./_types";
+import OrderTable from "./_components/order-table";
 
-function page() {
+const page = async () => {
+  const orders: OrderT[] = await getAllSellProduct();
   return (
-    <div>
-      Category Report
+    <div className="rounded-xl p-4 overflow-hidden bg-gradient-to-br from-white via-white to-white/40">
+      <OrderTable orders={orders} />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
+
